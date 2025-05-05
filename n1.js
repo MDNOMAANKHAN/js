29430815124f380ba7e1849a9149b5c13a795be5
MDNOMAANKHAN <123858810+MDNOMAANKHAN@users.noreply.github.com>
2025-05-05 14:54:12 +0530

    Delete n1.js

--- n1.js
+++
@@ -1,8 +0,0 @@
-const accountId = 123123
-let accountEmail="mohd121@GMAIL.com"
-let accountPassword="14141"
-accountcity="hyd"
-accountEmail = "mohd123@gamil.com"
-console.table([accountId,accountEmail,accountPassword,accountcity])
-
-// 
