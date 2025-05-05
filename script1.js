c3a31db25384aa52a78b3bf11babfc295804a61b
MDNOMAANKHAN <123858810+MDNOMAANKHAN@users.noreply.github.com>
2025-05-05 14:53:21 +0530

    Delete script1.js

--- script1.js
+++
@@ -1,8 +0,0 @@
-// Get the button and heading elements from the DOM
-const button = document.getElementById("changeTextBtn");
-const heading = document.getElementById("heading");
-
-// Add an event listener to the button
-button.addEventListener("click", function() {
-  heading.textContent = "ok done!";
-});
